0c96f870146de8eba8f6816bf5c6fece9ac84b9f
Axer Ivant <91627586+axerivant@users.noreply.github.com>
2022-11-24 04:22:31 -0800

    added homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { goto } from '$app/navigation'
+</script>
+
+<main class="bg-700 h-screen flex flex-col items-center justify-center">
+	<div class="w-86 flex flex-col gap-3 mb-24">
+		<div class="w-full text-white opacity-80">
+			This is the demo version of Snippy. All data is saved to browser local storage. <br /> To
+			reset application data, simply hard refresh.
+		</div>
+		<button on:click={() => goto('/a')} class="bg-500 text-white h-fit w-fit p-3 px-6 rounded">
+			start demo
+		</button>
+	</div>
+</main>
