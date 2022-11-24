8ba801cd76a83b524f6fbadf87d23c3c0a0f4e5a
Axer Ivant <91627586+axerivant@users.noreply.github.com>
2022-11-24 05:31:16 -0800

    _

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,6 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
-// and what to do when importing types
-declare namespace App {
-	// interface Locals {}
-	// interface PageData {}
-	// interface Error {}
-	// interface Platform {}
+interface Snip {
+	content: string
+	createdOnDate: Date
+	isDiscarded: boolean
+	id: string
 }
--- src/routes/a/+page.svelte
+++ src/routes/a/+page.svelte
@@ -1,2 +1,75 @@
-<script>
+<script lang="ts">
+	let snips: Snip[] = [
+		{
+			content:
+				'1 Lorem ipsum dolor sit amet consectetur. Consectetur sed velit urna eget ipsum. Urna nibh mattis pulvinar nisi urna. Tincidunt elementum sed scelerisque nisl. Vestibulum viverra libero malesuada turpis porttitor ultrices.',
+			createdOnDate: new Date(),
+			isDiscarded: false,
+			id: ''
+		},
+		{
+			content:
+				'2 Tincidunt elementum sed scelerisque nisl. Vestibulum viverra libero malesuada AS sturpis porttitor ultrices.',
+			createdOnDate: new Date(),
+			isDiscarded: false,
+			id: ''
+		},
+		{
+			content:
+				'3 Tincidunt elementum sed scelerisque nisl. Vestibulum viverra libero malesuada turpis porttisssssstor ultrices. Tisncidunt elementum AXER viverra libero malesuada turpis porttitor ultrices.',
+			createdOnDate: new Date(),
+			isDiscarded: false,
+			id: ''
+		},
+		{
+			content:
+				'4 Urna nibh mattis pulvinar nisi urna. Tincidunt elementum sed scelerisque nisl. Vestibulum viverra libero malesuada turpis porttitor ultrices.',
+			createdOnDate: new Date(),
+			isDiscarded: false,
+			id: ''
+		},
+		{
+			content: '5 Lorem ipsum dolor sit aitor ultrices.',
+			createdOnDate: new Date(),
+			isDiscarded: false,
+			id: ''
+		},
+		{
+			content:
+				'6 Tincidunt elementum sed scelerisque nisl. Vestibulum viverra libero malesuada AS sturpis porttitor ultrices.',
+			createdOnDate: new Date(),
+			isDiscarded: false,
+			id: ''
+		},
+		{
+			content:
+				'7 Tincidunt elementum sed scelerisque nisl. Vestibulum viverra libero malesuada turpis porttisssssstor ultrices. Tisncidunt elementum AXER viverra libero malesuada turpis porttitor ultrices.',
+			createdOnDate: new Date(),
+			isDiscarded: false,
+			id: ''
+		},
+		{
+			content:
+				'8 Lorem ipsum dolor sit amet consectetur. Consectetur sed velit urna eget ipsum. Urna nibh mattis pulvinar nisi urna. Tincidunt elementum sed scelerisque nisl.',
+			createdOnDate: new Date(),
+			isDiscarded: false,
+			id: ''
+		}
+	]
 </script>
+
+<main class="bg-700 h-screen text-white flex flex-col items-center justify-between p-12">
+	<div class="bg-white p-1 w-[50rem] max-h-[50rem] gap-3 columns-2">
+		{#each snips as snip}
+			<div
+				class="bg-[#77FF85] p-3 rounded-sm h-fit mb-3 break-inside-avoid [column-fill_balance]"
+			>
+				<p class="text-black opacity-90 w-full h-fit">{snip.content}</p>
+				<div>
+					<div>{snip.createdOnDate.toLocaleDateString()}</div>
+				</div>
+			</div>
+		{/each}
+	</div>
+	<div>CREATE</div>
+</main>
